2e1d6baa8b2fbda84e097eafe48d852a197e7e6b
TheStonedRaider <joebopie@googlemail.com>
2019-05-06 18:06:32 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,20 +1,27 @@
+-- Made by The Stoned raider 
+--Check out our server DOJSRC
 
 
 ESX = nil
-
+local PlayerData                = {}
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(4)
-	end
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
 end)
-
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  PlayerData.job = job
+end)
 
 Citizen.CreateThread(function()
 	while true do
 		Wait(10)
-		if IsControlJustPressed(1, Config.Key2) and IsControlPressed(1, Config.Key1) then
-			OpenRadioMenu()
+
+if IsControlJustPressed(1, Config.Key2) and IsControlPressed(1, Config.Key1)   then
+			OpenRadioMenu()	
+			
 			end
 	end
 end)
@@ -24,15 +31,27 @@ end)
 function OpenRadioMenu()
 
   local elements = {
-    {label = _U('leave'), value = 'leave'},
-    {label = _U('R1'), value = 'radio1'},
-    {label = _U('R2'), value = 'radio2'},
-    {label = _U('R3'), value = 'radio3'},	
-    {label = _U('R4'), value = 'radio4'},	
-    {label = _U('R5'), value = 'radio5'}	
+    {label = _U('leavepublic'), value = 'leavepublic'}
 	
 }
 
+
+    if (PlayerData.job.name == 'police' or PlayerData.job.name == 'fire' or PlayerData.job.name == 'ambulance') then
+     
+      table.insert(elements, { label = _U('leavepolice'), value = 'leavepolice'})  
+      table.insert(elements, { label = _U('R1'), value = 'radio6'})  
+      table.insert(elements, { label = _U('R2'), value = 'radio7'})  
+      table.insert(elements, { label = _U('R3'), value = 'radio8'}) 
+	  table.insert(elements, { label = _U('R4'), value = 'radio9'})  
+      table.insert(elements, { label = _U('R5'), value = 'radio10'})  
+	  
+end	
+      table.insert(elements, { label = _U('CB1'), value = 'radio1'})  
+      table.insert(elements, { label = _U('CB2'), value = 'radio2'})  
+      table.insert(elements, { label = _U('CB3'), value = 'radio3'}) 
+	  table.insert(elements, { label = _U('CB4'), value = 'radio4'})  
+      table.insert(elements, { label = _U('CB5'), value = 'radio5'})  
+      table.insert(elements, { label = _U('AD'), value = 'nothing'}) 
   ESX.UI.Menu.CloseAll()
 
 
@@ -47,54 +66,97 @@ function OpenRadioMenu()
         function(data, menu)
 
       menu.close()
-      if data.current.value == 'leave' then
-          TriggerEvent('TokoVoip:removePlayerFromRadio','Call PD Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','EMS Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO/EMS Shared Radio')
-         TriggerEvent('TokoVoip:removePlayerFromRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO Shared Radio')     
-      end     
+      if data.current.value == 'leavepublic' then
+          exports.tokovoip_script:removePlayerFromRadio(11)
+          exports.tokovoip_script:removePlayerFromRadio(12)
+          exports.tokovoip_script:removePlayerFromRadio(13)
+          exports.tokovoip_script:removePlayerFromRadio(14)
+          exports.tokovoip_script:removePlayerFromRadio(15)
+      end 
+      if data.current.value == 'leavepolice' then
+          exports.tokovoip_script:removePlayerFromRadio(16)
+          exports.tokovoip_script:removePlayerFromRadio(17)
+          exports.tokovoip_script:removePlayerFromRadio(18)
+          exports.tokovoip_script:removePlayerFromRadio(19)
+          exports.tokovoip_script:removePlayerFromRadio(20)
+      end 	  
       if data.current.value == 'radio1' then
-          TriggerEvent('TokoVoip:addPlayerToRadio','Call PD Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','EMS Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO/EMS Shared Radio')
-         TriggerEvent('TokoVoip:removePlayerFromRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO Shared Radio')     
+		  exports.tokovoip_script:addPlayerToRadio(11 )
+          exports.tokovoip_script:removePlayerFromRadio(12 )
+          exports.tokovoip_script:removePlayerFromRadio(13 )
+          exports.tokovoip_script:removePlayerFromRadio(14 )
+          exports.tokovoip_script:removePlayerFromRadio(15 )    
       end
       if data.current.value == 'radio2' then
-          TriggerEvent('TokoVoip:addPlayerToRadio','EMS Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO/EMS Shared Radio')
-         TriggerEvent('TokoVoip:removePlayerFromRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO Shared Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','Call PD Radio')      
+		  exports.tokovoip_script:addPlayerToRadio(12 )
+          exports.tokovoip_script:removePlayerFromRadio(11 )
+          exports.tokovoip_script:removePlayerFromRadio(13 )
+          exports.tokovoip_script:removePlayerFromRadio(14 )
+          exports.tokovoip_script:removePlayerFromRadio(15 )   
       end
       if data.current.value == 'radio3' then
-          TriggerEvent('TokoVoip:addPlayerToRadio','PD/SO/EMS Shared Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','EMS Radio')
-         TriggerEvent('TokoVoip:removePlayerFromRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO Shared Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','Call PD Radio') 
+		  exports.tokovoip_script:addPlayerToRadio(13 )
+          exports.tokovoip_script:removePlayerFromRadio(11 )
+          exports.tokovoip_script:removePlayerFromRadio(12 )
+          exports.tokovoip_script:removePlayerFromRadio(14 )
+          exports.tokovoip_script:removePlayerFromRadio(15 )
 
       end	 
       if data.current.value == 'radio4' then
-          TriggerEvent('TokoVoip:addPlayerToRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','Call PD Radio') 
-          TriggerEvent('TokoVoip:removePlayerFromRadio','EMS Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO Shared Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO/EMS Shared Radio')
+		  exports.tokovoip_script:addPlayerToRadio(14 )
+          exports.tokovoip_script:removePlayerFromRadio(11 )
+          exports.tokovoip_script:removePlayerFromRadio(12 )
+          exports.tokovoip_script:removePlayerFromRadio(13 )
+          exports.tokovoip_script:removePlayerFromRadio(15 )
       end
       if data.current.value == 'radio5' then
-          TriggerEvent('TokoVoip:addPlayerToRadio','PD/SO Shared Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','Call PD Radio') 
-          TriggerEvent('TokoVoip:removePlayerFromRadio','EMS Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','SO Radio')
-          TriggerEvent('TokoVoip:removePlayerFromRadio','PD/SO/EMS Shared Radio')
+		  exports.tokovoip_script:addPlayerToRadio(15 )
+          exports.tokovoip_script:removePlayerFromRadio(11 )
+          exports.tokovoip_script:removePlayerFromRadio(12 )
+          exports.tokovoip_script:removePlayerFromRadio(13 )
+          exports.tokovoip_script:removePlayerFromRadio(14 )
 
       end
-	  
+      if data.current.value == 'radio6' then
+		  exports.tokovoip_script:addPlayerToRadio(16 )
+          exports.tokovoip_script:removePlayerFromRadio(17 )
+          exports.tokovoip_script:removePlayerFromRadio(18 )
+          exports.tokovoip_script:removePlayerFromRadio(19 )
+          exports.tokovoip_script:removePlayerFromRadio(20 )
 
-     
+      end	  
+      if data.current.value == 'radio7' then
+		  exports.tokovoip_script:addPlayerToRadio(17 )
+          exports.tokovoip_script:removePlayerFromRadio(16 )
+          exports.tokovoip_script:removePlayerFromRadio(18 )
+          exports.tokovoip_script:removePlayerFromRadio(19 )
+          exports.tokovoip_script:removePlayerFromRadio(20 )
 
+      end	
+      if data.current.value == 'radio8' then
+		  exports.tokovoip_script:addPlayerToRadio(18 )
+          exports.tokovoip_script:removePlayerFromRadio(16 )
+          exports.tokovoip_script:removePlayerFromRadio(17 )
+          exports.tokovoip_script:removePlayerFromRadio(19 )
+          exports.tokovoip_script:removePlayerFromRadio(20 )
+
+      end     
+      if data.current.value == 'radio9' then
+		  exports.tokovoip_script:addPlayerToRadio(19 )
+          exports.tokovoip_script:removePlayerFromRadio(16 )
+          exports.tokovoip_script:removePlayerFromRadio(17 )
+          exports.tokovoip_script:removePlayerFromRadio(18 )
+          exports.tokovoip_script:removePlayerFromRadio(20 )
+
+      end 
+      if data.current.value == 'radio10' then
+		  exports.tokovoip_script:addPlayerToRadio(20 )
+          exports.tokovoip_script:removePlayerFromRadio(16 )
+          exports.tokovoip_script:removePlayerFromRadio(17 )
+          exports.tokovoip_script:removePlayerFromRadio(19 )
+          exports.tokovoip_script:removePlayerFromRadio(18 )
+
+      end 
       CurrentAction     = 'radio'
       CurrentActionMsg  = _U('open_cloackroom')
       CurrentActionData = {}
